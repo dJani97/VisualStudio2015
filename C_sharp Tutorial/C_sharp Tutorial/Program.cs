761b22ddf52523ac9285943c2f5d99754b2953a4
dJani97 <dobszaijani@gmail.com>
2016-12-28 13:06:52 +0100

    trying

---
+++ C_sharp Tutorial/C_sharp Tutorial/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_sharp_Tutorial
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
